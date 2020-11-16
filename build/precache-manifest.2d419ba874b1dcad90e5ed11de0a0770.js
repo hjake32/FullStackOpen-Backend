98fadee723b703733e8e0d017025ca64edd1b757
Jake Henkel <h.jake32@gmail.com>
2020-11-16 11:24:59 -0500

    Add production build to directory

---
+++ build/precache-manifest.2d419ba874b1dcad90e5ed11de0a0770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5944f536c60aca20748a21893aedcd92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a825b186c8f6d98c66d4",
+    "url": "/static/css/main.bd7c78a4.chunk.css"
+  },
+  {
+    "revision": "28dcfbfd5ed22daa4d09",
+    "url": "/static/js/2.c3fc0fe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3fc0fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a825b186c8f6d98c66d4",
+    "url": "/static/js/main.ffd37c50.chunk.js"
+  },
+  {
+    "revision": "3d09fc7fbfc0228634fe",
+    "url": "/static/js/runtime-main.7266208d.js"
+  }
+]);
